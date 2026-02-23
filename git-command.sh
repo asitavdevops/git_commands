9f46d4556e1344684b99d60bbd767275674b3e29
Asitav attanaik <git.asitav@gmail.com>
2026-02-23 13:28:52 +0530

    git V2

--- git-command.sh
+++ git-command.sh
@@ -3,3 +3,5 @@ git commit -m "Version1"
 git push
 git diff - to check the difference between old file content and new changes to the file 
 git logs - check the different commits
+git remote add origin https://github.com/asitavdevops/git_commands.git - To Add Origin from your local
+git remote -v - it will display the origin and main url
