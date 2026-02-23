1b991478ba360e27d6da6b4cc3adc10e8ae06c4e
Asitav attanaik <git.asitav@gmail.com>
2026-02-23 13:48:45 +0530

    git v3

--- git-command.sh
+++ git-command.sh
@@ -5,3 +5,5 @@ git diff - to check the difference between old file content and new changes to t
 git logs - check the different commits
 git remote add origin https://github.com/asitavdevops/git_commands.git - To Add Origin from your local
 git remote -v - it will display the origin and main url
+git pull origin main - To pull remote changes first to your local 
+git push origin main --rebase - It will pull remote changes to local , apply remote changes , reapply local commits on top
